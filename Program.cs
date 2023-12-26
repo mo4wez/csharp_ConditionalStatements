b202ba1541ae959bade34f0306f282300cf02e04
moawezz <moawez.de@gmail.com>
2023-12-27 00:29:28 +0330

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter start point:");
+            string start = Console.ReadLine();
+            int startInt = Convert.ToInt32(start);
+            Console.Write("Enter end point:");
+            string end = Console.ReadLine();
+            int endInt = Convert.ToInt32(end);
+
+            if (endInt < startInt)
+            {
+                Console.WriteLine("Error: end point should be greater than start point.");
+            }
+            else 
+            {
+                int[] result = OneToTenWithInput(startInt, endInt);
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.WriteLine(result[i]);
+                }
+            }
+        }
+
+        static int[] OneToTenWithInput(int start, int end)
+        {
+            int arraySize = end - start + 1;
+            int counter = 0;
+            int[] numsArray = new int[arraySize];
+            for (int i = start; i <= end; i++) 
+            {
+                numsArray[counter] = i;
+                counter++;
+            }
+            return numsArray;
+        }
+    }
+}
